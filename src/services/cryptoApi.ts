e4497763408dcf53ee651840101ffac5274de6c3
akshat-dahiya10 <dahiyaakshat10@gmail.com>
2026-06-18 09:53:26 +0530

    Update cryptoApi.ts

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -1,17 +1,15 @@
-export const fetchCoins = async () => {
-  try {
-    const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
-    );
+export const getTopGainer = async () => {
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=percent_change_24h_desc'
+  );
+  const data = await res.json();
+  return data[0];
+};
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("API ERROR:", error);
-    return [];
-  }
+export const searchCoins = async (query: string) => {
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/search?query=${query}`
+  );
+  const data = await res.json();
+  return data.coins;
 };
